2c1b1d186d5fa931ac9fef18166176f726c178a4
omosteven <omosteven123@gmail.com>
2025-01-27 09:53:11 +0100

    extended payload size limit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from app.routes import routes  # Import routes
 from app.constants.constants import API_VERSION
 
 app = FastAPI()
+
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+
+class MaxSizeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        max_size = 500 * 1024 * 1024  # 10 MB
+        if request.headers.get('content-length') and int(request.headers['content-length']) > max_size:
+            return Response("Request entity too large", status_code=413)
+        return await call_next(request)
+
+app.add_middleware(MaxSizeMiddleware)
 
 # Include routes
 app.include_router(routes.router, prefix=API_VERSION)
