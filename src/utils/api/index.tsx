7b1758fe869161dc8dd13d38c3658d1937ca0044
omosteven <omosteven123@gmail.com>
2025-02-13 11:02:46 +0100

    test socket on secure route

--- src/utils/api/index.tsx
+++ src/utils/api/index.tsx
@@ -2,7 +2,7 @@
 
 // export const socketBaseUrl = "ws://localhost:8000/api/v1/ws/"
 
-export const socketBaseUrl = "ws://18.198.217.46:8000/api/v1/ws/"
+export const socketBaseUrl = "wss://18.198.217.46/api/v1/ws/"
 // export const socketBaseUrl = "wss://18.198.217.46/ws";
 // "ws://https://18.198.217.46/api/v1/ws"
 
